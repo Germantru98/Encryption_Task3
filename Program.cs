dae91e95fa728ec67d03455ca6705e185b313f67
Germantru98 <germantruhanov@gmail.com>
2019-09-13 20:48:54 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DataPharser dp = new DataPharser();
+            string str = "aavfghdsh fsagijpo oito aagjgj ooffaadasd";
+            var table = new Dictionary<string, int>();
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                var tmp = str[i].ToString() + str[i + 1].ToString();
+                if (!table.ContainsKey(tmp))
+                {
+                    table.Add(tmp, 0);
+                }
+            }
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                var tmp = str[i].ToString() + str[i + 1].ToString();
+                if(table.ContainsKey(tmp))
+                {
+                    table[tmp]++;
+                }
+            }
+            foreach (var item in table)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
