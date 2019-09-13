6cab35df145dbf18bc4f07a071d14a18c2cd4b4a
Germantru98 <germantruhanov@gmail.com>
2019-09-13 21:18:37 +0400

    fixed 1

--- Program.cs
+++ Program.cs
@@ -10,25 +10,23 @@ namespace Encryption_Task3
     {
         static void Main(string[] args)
         {
-            //DataPharser dp = new DataPharser();
-            string str = "aavfghdsh fsagijpo oito aagjgj ooffaadasd";
+            DataPharser dp = new DataPharser();
+            string str = dp.getMessage("Message.txt");
             var table = new Dictionary<string, int>();
             for (int i = 0; i < str.Length-1; i++)
             {
                 var tmp = str[i].ToString() + str[i + 1].ToString();
-                if (!table.ContainsKey(tmp))
-                {
-                    table.Add(tmp, 0);
-                }
-            }
-            for (int i = 0; i < str.Length-1; i++)
-            {
-                var tmp = str[i].ToString() + str[i + 1].ToString();
-                if(table.ContainsKey(tmp))
+                if (table.ContainsKey(tmp))
                 {
                     table[tmp]++;
                 }
+                else
+                {
+                    table.Add(tmp, 1);
+                }
+                
             }
+           
             foreach (var item in table)
             {
                 Console.WriteLine(item);
